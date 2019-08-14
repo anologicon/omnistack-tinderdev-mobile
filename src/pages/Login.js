fa5d62aba77b6a3a16f7d36b85b9d6152396743f
Anologicon <pauloraimundi2009@gmail.com>
2019-08-14 00:19:22 -0300

    Mobile

---
+++ src/pages/Login.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import { 
+    KeyboardAvoidingView,
+    Platform,
+    StyleSheet,
+    Image,
+    Text,
+    TextInput,
+    TouchableOpacity
+ } from 'react-native';
+
+import api from '../services/api';
+import logo from '../assets/logo.png';
+
+export default function Login({ navigation }) {
+    const [user, setUser] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if(user) {
+                navigation.navigate('Main', { user })
+            }
+        });
+    }, []);
+
+    async function handleLogin() {
+        
+        try {
+            const response = await api.post('/devs', {
+                username : user
+                
+            });
+
+            const { _id } = response.data;
+
+            await AsyncStorage.setItem('user', JSON.stringify({ user: _id }));
+
+            console.log(_id);
+
+            navigation.navigate('Main', { user: _id });
+        } catch (error) {
+            console.error(error);
+        }
+            
+    }
+    
+    return (
+        <KeyboardAvoidingView
+            behavior="padding"
+            enabled={Platform.OS == 'ios'}
+            style={styles.container}
+        >
+            <Image source={logo} />
+            
+            <TextInput
+                autoCorrect={false}
+                autoCapitalize="none"
+                style={styles.input}
+                value={user}
+                placeholderTextColor="#999"
+                placeholder="Digite seu usuário no GitHub"
+                onChangeText={setUser}
+            />
+
+            <TouchableOpacity onPress={handleLogin} style={styles.button}>
+                <Text style={styles.buttonText}>Enviar</Text>
+            </TouchableOpacity>
+        </KeyboardAvoidingView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30,
+    },
+
+    input: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#DDD',
+        borderRadius: 4,
+        marginTop: 20,
+        paddingHorizontal: 15 
+    },
+
+    button: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#DF4723',
+        borderRadius: 4,
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    buttonText:{
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+})
