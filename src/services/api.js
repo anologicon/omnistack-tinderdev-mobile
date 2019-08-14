d475a750e7ff78e7a44d2dcf97f837f50b5c6946
Anologicon <pauloraimundi2009@gmail.com>
2019-08-14 13:00:57 -0300

    Add heroko api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.109:7100'
+    baseURL: 'https://anomnistackbackend.herokuapp.com/'
 });
 
 export default api;
